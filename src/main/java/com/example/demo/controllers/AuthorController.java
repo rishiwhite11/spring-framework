3be46ac2ca3c134c85faa4d2dd731c051bc83732
rishiwhite11 <rishiwhite@gmail.com>
2017-11-09 12:10:47 +0530

    Assignment done for fetching the authors

--- src/main/java/com/example/demo/controllers/AuthorController.java
+++ src/main/java/com/example/demo/controllers/AuthorController.java
@@ -1,8 +1,26 @@
 package com.example.demo.controllers;
 
+import com.example.demo.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Bubul on 09-11-2017.
  */
+@Controller
 public class AuthorController {
+    @Autowired
+    private AuthorRepository authorRepository;
+
+//    public AuthorController(AuthorRepository authorRepository) {
+//        this.authorRepository = authorRepository;
+//    }
+    @RequestMapping("/authors")
+    public String author(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
 
 }
